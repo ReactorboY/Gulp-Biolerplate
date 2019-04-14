2bcbe809468775c35af2820b9339f5fbc511aa2d
Muhammad Hussain <samsteve124@gmail.com>
2019-04-14 21:44:51 +0530

    images minified added wih newer

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ const gulp = require('gulp');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
+
+const newer = require('gulp-newer');
 const browserSync = require('browser-sync').create();
 
 const folders = {
@@ -9,14 +12,22 @@ const folders = {
     build: 'public/'
 };
 
-gulp.task('css', () => {
+//  minify any new images
+gulp.task('images', () => {
+    return gulp.src(folders.src + 'images/**/*')
+        // .pipe(newer(folders.src + 'images/'))
+        .pipe(imagemin({optimizationLevel: 5}))
+        .pipe(gulp.dest(folders.build + 'images/'));
+})
+
+gulp.task('css', gulp.series('images'),() => {
     let postOpt = [
         autoprefixer({
             browsers: ['last 2 versions', '> 2%']
         })
     ];
 
-    return gulp.src(folders.src+ 'sass/main.scss')
+    return gulp.src(folders.src + 'sass/main.scss')
         .pipe(sass({
             outputStyle: 'nested',
             precision: 3,
